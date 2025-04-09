8e615aed0ae03af28f5230badaf821c1f0b1fc98
luiz <144607216+pnzrfst@users.noreply.github.com>
2025-04-09 16:35:43 -0300

    Update test_fatorial.py

--- test_fatorial.py
+++ test_fatorial.py
@@ -1,7 +1,7 @@
 import pytest 
 from fatorial import fatorial
 
-@pytest.mark.parametrize("factor, expected", [(3, 6),(4, 24),(5, 120),(6, 720)])
+@pytest.mark.parametrize("fator, expected", [(3, 6),(4, 24),(5, 120),(6, 720)])
 
-def test_factor(factor, expected):
-    assert fatorial(factor) == expected
+def test_factor(fator, expected):
+    assert fatorial(fator) == expected
